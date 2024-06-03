bb4e2258978cfef5c82bc522bab4c5b2417b111c
rdata08 <ryanigomez4@gmail.com>
2024-06-03 18:07:32 -0400

    Create automate_analysis.py

---
+++ automate_analysis.py
@@ -0,0 +1,47 @@
+import csv
+from textblob import TextBlob
+
+def avg_question_count_by_race(filename):
+    with open(filename, 'r') as voirdiredata:
+        csv_reader = csv.reader(voirdiredata)
+        
+        # skips header of table
+        next(csv_reader)
+
+        hashMap = {}
+
+        # quantifies number of questions asked to ind.
+        totalCount = 0
+
+        for row in csv_reader:
+            race = row[2]
+            q_count = int(row[7])
+            totalCount += q_count
+            if race in hashMap:
+                hashMap[race] += q_count
+            else:
+                hashMap[race] = q_count
+       
+        ans = {}
+        for race in hashMap:
+            q_count = hashMap[race] 
+            ans[race] = q_count / totalCount
+            
+        print(ans)
+        return ans
+
+def sentiment_by_race(filename):
+    with open(filename, 'r') as voirdiredata:
+        csv_reader = csv.reader(voirdiredata)
+        
+        # skips header of table
+        next(csv_reader)  
+        
+        for row in csv_reader:
+            question = row[6]
+            blob = TextBlob(question)
+            sentiment = blob.sentiment
+            print(sentiment)
+            
+avg_question_count_by_race('voirdiredata.csv')
+sentiment_by_race('voirdiredata.csv')
